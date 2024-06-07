13f6e5c6093cc3bd22342996193152d6250a0cd8
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2024-06-07 14:46:09 -0500

    Delete src directory

--- src/main/resources/db/migration/V1__create-table-usuarios.sql
+++
@@ -1,9 +0,0 @@
-
-create table usuarios(
-    id bigint not null auto_increment,
-    nombre varchar(200) not null,
-    correo varchar(200) not null unique,
-    contraseña varchar(300) not null,
-
-    primary key(id)
-);
