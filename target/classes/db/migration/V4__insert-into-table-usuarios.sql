6170a439cee977a4688d3697f49f089a01c29e0b
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2024-06-14 19:15:11 -0500

    Add files via upload

---
+++ target/classes/db/migration/V4__insert-into-table-usuarios.sql
@@ -0,0 +1,8 @@
+INSERT INTO `foro_hub_pru`.`usuarios` (`id`, `nombre`, `correo`, `contraseña`)
+VALUES ('1', 'amanda', 'a@mail.com', '$2a$10$msWgvAfuotCY.CCvNU1lQeWx7nwZ7s2tefpL8AvyPJ62/CjXguWsm');
+INSERT INTO `foro_hub_pru`.`usuarios` (`id`, `nombre`, `correo`, `contraseña`)
+VALUES ('2', 'bruna', 'b@mail.com', '$2a$10$MqdBTac/lSLw4kvGdcC6VeGgpNEoWa4c1Bjr3x3kulXLmeAe3PG0i');
+INSERT INTO `foro_hub_pru`.`usuarios` (`id`, `nombre`, `correo`, `contraseña`)
+VALUES ('3', 'carla', 'c@mail.com', '$2a$10$sYdSXjp70uNYGvMwPYQy0.82DS/NmYl9O2epXX827UYrGEXaDRgWC');
+INSERT INTO `foro_hub_pru`.`usuarios` (`id`, `nombre`, `correo`, `contraseña`)
+VALUES ('4', 'debora', 'd@mail.com', '$2a$10$Qc/kV7iW1gHQNBY0qKxg/ei9iMzMnaEMp0aSMZd.GLiYrl2qAdANq');
